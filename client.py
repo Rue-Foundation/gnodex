79af4f96023234edca6464f5cd2b69d6a8984cf1
Rami Khalil <vioronic@gmail.com>
2017-03-27 14:00:36 +0200

    Sign orders

--- client.py
+++ client.py
@@ -1,19 +1,44 @@
-import socket, ssl, pprint, pickle
+import socket, ssl, pprint, pickle, sys
+from ethereum import utils
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Signature import PKCS1_v1_5
+from Cryptodome.Hash import SHA256
 
+# Open SSL Connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 ssl_sock = ssl.wrap_socket(sock,
                            ca_certs="certs/server.crt",
                            cert_reqs=ssl.CERT_REQUIRED,
                            ssl_version=ssl.PROTOCOL_TLSv1_2)
-
 ssl_sock.connect(('localhost', 31337))
 
+# Load public key for signature verification
+cert = RSA.importKey(open('certs/server.crt', 'rb').read())
+pkcs = PKCS1_v1_5.new(cert)
+
+# Just print some debugging data
 print(ssl_sock.getpeername())
 print(pprint.pformat(ssl_sock.getpeercert()))
 print(ssl_sock.cipher())
 
+# Get user input, send to server
+while True:
+    # Read input line, and send as RLP
+    order = sys.stdin.readline()
+    rlp_encoded = utils._encode_hex(order)
+    # TODO: Encrypt order with DKG Key
+    ssl_sock.send(pickle.dumps(rlp_encoded))
+    print("SENT: " + rlp_encoded)
+    # Receive signature from state server
+    resp = pickle.loads(ssl_sock.recv()) # TODO: Safe object loading
+    print("RECV: " + str(resp))
+    # Verify Signature
+    hash = SHA256.new(str(rlp_encoded).encode('utf-8'))
+    try:
+        pkcs.verify(hash, resp)
+        print("SIGNATURE OK!")
+    except ValueError:
+        print("SIGNATURE FAILED!!")
 
-ssl_sock.send(pickle.dumps("Test"))
 
 ssl_sock.close()
--- server.py
+++ server.py
@@ -1,19 +1,36 @@
-import socket, ssl, pickle
+import socket, ssl, pickle, _thread
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Signature import PKCS1_v1_5
+from Cryptodome.Hash import SHA256
 
+# Start listening for connections
 sock = socket.socket()
 sock.bind(('', 31337))
 sock.listen()
 
-while True:
-    newsock, addr = sock.accept()
-    ssl_sock = ssl.wrap_socket(newsock,
+# Load private key for signatures
+key = RSA.import_key(open('certs/server.key', 'rb').read())
+pkcs = PKCS1_v1_5.new(key)
+
+
+# One thread per client
+def handle_client(sock, addr):
+    ssl_sock = ssl.wrap_socket(sock,
                                server_side= True,
                                certfile='certs/server.crt',
                                keyfile='certs/server.key',
                                ssl_version=ssl.PROTOCOL_TLSv1_2)
-
-    try:
-        data = pickle.loads(ssl_sock.recv())
+    # Wait for input, and respond
+    while True:
+        data = pickle.loads(ssl_sock.recv()) # TODO: Safe object loading
         print("INPUT: " + data)
-    finally:
-        ssl_sock.close()
+        # Hash and sign
+        hash = SHA256.new(str(data).encode('utf-8'))
+        resp = pkcs.sign(hash)
+        ssl_sock.send(pickle.dumps(resp))
+        print("RESP: " + str(resp))
+
+# Accept connections and start handling them in own thread
+while True:
+    newsock, addr = sock.accept()
+    _thread.start_new_thread(handle_client, (newsock, addr))
