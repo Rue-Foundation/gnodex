7703f73a2a44abd3fb7607b5e530b429f3593fe8
Rami Khalil <vioronic@gmail.com>
2017-03-27 16:12:24 +0200

    Batch RLP Structure

--- order.py
+++ order.py
@@ -1,7 +1,13 @@
 import rlp
-from rlp.sedes import raw
+from rlp.sedes import raw, CountableList
 
 class Order(rlp.Serializable):
-    fields =[
+    fields = [
         ('raw', raw),
+    ]
+
+
+class Batch(rlp.Serializable):
+    fields = [
+        ('orders', CountableList(Order)),
     ]
