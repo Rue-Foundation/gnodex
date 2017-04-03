d99aab001562d49238dcd46565e670cfa0690d75
Rami Khalil <the.rami.khalil@gmail.com>
2017-04-03 08:59:01 +0200

    Signature, Signed Batch models

--- gnodex/models/batch.py
+++ gnodex/models/batch.py
@@ -1,9 +1,16 @@
 import rlp
-from rlp.sedes import CountableList, BigEndianInt
+from rlp.sedes import CountableList, BigEndianInt, raw
 from .order import Order
+from .crypto import Signature
 
 class Batch(rlp.Serializable):
     fields = [
         ('round', BigEndianInt(8)),
         ('orders', CountableList(Order)),
+    ]
+
+class SignedBatch(rlp.Serializable):
+    fields = [
+        ('signatures', CountableList(Signature)),
+        ('batch', Batch)
     ]
---
+++ gnodex/models/crypto.py
@@ -0,0 +1,8 @@
+import rlp
+from rlp.sedes import raw
+
+class Signature(rlp.Serializable):
+    fields = [
+        ('owner_id', raw),
+        ('signature', raw),
+    ]
