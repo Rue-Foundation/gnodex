2aafa5f64c141df4e1b11b1a22cff063c5bd5eda
Rami Khalil <the.rami.khalil@gmail.com>
2017-04-17 16:52:22 +0200

    Matching model

--- gnodex/matcher/service.py
+++ gnodex/matcher/service.py
@@ -1,9 +1,10 @@
 import certs
 import matcher
+import rlp
 import threading
 from util.ssl_sock_helper import ssl_connect
-from util.rpc import rpc_call
-from models import Batch
+from util.rpc import rpc_call_rlp
+from models import SignedBatch
 
 
 def batch_matcher_service(args):
@@ -14,7 +15,7 @@ def batch_matcher_service(args):
 
 def request_batch():
     print("REQUESTING BATCH")
-    batch = None
+    signed_batch = None
     try:
         try:
             ssl_sock = ssl_connect(('localhost', 31337), certs.path_to('server.crt'))
@@ -22,18 +23,20 @@ def request_batch():
             print("CONNECTION ERROR")
             return
         with ssl_sock:
-            batch = rpc_call(ssl_sock, "return_latest_signed_batch", {}, default_timeout=True)
+            signed_batch_rlp = rpc_call_rlp(ssl_sock, "return_latest_signed_batch", {}, default_timeout=True)
+            signed_batch = rlp.decode(signed_batch_rlp, SignedBatch)
     except TimeoutError:
         print("REQUEST TIMED OUT")
-    finally:
-        if not batch:
-            print("NO BATCH AVAILABLE YET")
-            t = threading.Timer(interval=2.0, function=request_batch)
-            t.start()
-            return
-        print("RECEIVED BATCH")
-        process_batch(batch)
+
+    if not signed_batch:
+        print("NO BATCH AVAILABLE YET")
+        t = threading.Timer(interval=2.0, function=request_batch)
+        t.start()
+        return
+    print("RECEIVED BATCH")
+    print(signed_batch.batch.orders)
+    process_batch(signed_batch)
 
 
-def process_batch(batch: Batch):
+def process_batch(batch: SignedBatch):
     pass
---
+++ gnodex/models/matching.py
@@ -0,0 +1,26 @@
+import rlp
+from rlp.sedes import CountableList, BigEndianInt, raw
+from .crypto import Signature
+
+
+class Route(rlp.Serializable):
+    fields = [
+        ('left_order', BigEndianInt(8)),
+        ('left_amount', BigEndianInt(8)),
+        ('right_order', BigEndianInt(8)),
+        ('right_amount', BigEndianInt(8)),
+    ]
+
+
+class Matching(rlp.Serializable):
+    fields = [
+        ('routes', CountableList(Route)),
+        ('batch_hash', raw)
+    ]
+
+
+class SignedMatching(rlp.Serializable):
+    fields = [
+        ('matching', Matching),
+        ('signatures', CountableList(Signature))
+    ]
