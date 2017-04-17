c07a5f621d683fba6feba267d2768b75574865ba
Rami Khalil <the.rami.khalil@gmail.com>
2017-04-17 12:17:50 +0200

    BMS Batch Request

--- gnodex/matcher/__init__.py
+++ gnodex/matcher/__init__.py
@@ -1,5 +1,22 @@
+import certs
 from .service import batch_matcher_service
+from util import crypto
+
+public_key = None
+private_key = None
+
+
+def init(args):
+    global public_key
+    global private_key
+
+    # Load public key for signature verification
+    public_key = crypto.load_public_cert_key(certs.path_to('server.crt'))
+    # Load private key for signatures
+    private_key = crypto.load_private_key(certs.path_to('server.key'))
+
+    return batch_matcher_service(args)
 
 
 def setup_argparser(parser):
-    parser.set_defaults(func=batch_matcher_service)
+    parser.set_defaults(func=init)
--- gnodex/matcher/service.py
+++ gnodex/matcher/service.py
@@ -1,4 +1,40 @@
+import certs
+import matcher
+import threading
+from util.ssl_sock_helper import ssl_connect
+from util.rpc import rpc_call
+from models import Batch
+
 
 def batch_matcher_service(args):
-    print("Hello, world!")
+    print("Gnodex Batch Matcher Service Started")
+    t = threading.Timer(interval=2.0, function=request_batch)
+    t.start()
+
+
+def request_batch():
+    print("REQUESTING BATCH")
+    batch = None
+    try:
+        try:
+            ssl_sock = ssl_connect(('localhost', 31337), certs.path_to('server.crt'))
+        except ConnectionError:
+            print("CONNECTION ERROR")
+            return
+        with ssl_sock:
+            batch = rpc_call(ssl_sock, "request_batch", {}, True)
+    except TimeoutError:
+        print("REQUEST TIMED OUT")
+    finally:
+        if not batch:
+            print("NO BATCH AVAILABLE YET")
+            t = threading.Timer(interval=2.0, function=request_batch)
+            t.start()
+            return
+        print("RECEIVED BATCH")
+        print(batch)
+        process_batch(batch)
+
+
+def process_batch(batch: Batch):
     pass
