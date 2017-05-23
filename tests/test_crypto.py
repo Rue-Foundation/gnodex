279234c9fedb38bf5dffec3b78e6b9fc39e07a60
Alan Lu <alanlu1023@gmail.com>
2017-05-23 12:52:14 -0500

    Added gas cost checking thing

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -6,10 +6,16 @@ from gnodex.util import crypto
 
 def test_can_decrypt_encrypted_random_messages(chain):
     ies, _ = chain.provider.get_or_deploy_contract('IntegratedEncryptionScheme')
-    for _ in range(100):
+    num_runs = 10
+    average_gas_cost = 0
+    for _ in range(num_runs):
         message = os.urandom(util.random.randrange(1, 100))
         deckey = util.random_private_value()
         enckey = bitcoin.fast_multiply(bitcoin.G, deckey)
         ciphertext = crypto.encrypt(message, enckey)
         assert message == crypto.decrypt(ciphertext, deckey)
         assert crypto.decrypt(ciphertext, deckey, foo=True) == ies.call().decrypt(ciphertext, deckey)
+        average_gas_cost += ies.estimateGas().decrypt(ciphertext, deckey)
+
+    average_gas_cost /= num_runs
+    # assert average_gas_cost == 0
